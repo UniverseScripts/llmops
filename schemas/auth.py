a646c3714d1da3289892216be2e0d203b9201bd3
Asterios <universegaming234@gmail.com>
2026-03-16 12:32:45 +0700

    Finished setting up Auth routers with Stripe account, user modifications to SQL schema.

--- schemas/auth.py
+++ schemas/auth.py
@@ -5,7 +5,7 @@ class UserCreate(BaseModel):
     email: EmailStr = Field(..., description="Contains user's email for registration.")
     password_input: str = Field(..., description="Contains user's password input for hashing.")
     
-class UserResponse(UserCreate):
+class UserResponse(BaseModel):
     username: str = Field(..., description="Contains username for response.")
     email: EmailStr = Field(..., description="Contains user's email for response.")
     message: str = Field(default="", description="Contains status code to notify whether registration was successful or not.")
